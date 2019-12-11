bed42d1547e00f18045c29f3b379cbd76cbcc00f
kou466 <1226hajing@gmail.com>
2019-12-11 18:22:56 +0900

    code

--- Test/Finals/A37.c
+++ Test/Finals/A37.c
@@ -4,41 +4,45 @@
 int main() {
 	int n, m, k, i, j, p, ** A, ** B, Cij;
 
-	scanf("%d %d", &n, &m);
+	scanf("%d %d %d", &n, &m, &k);
 
-	A = (int**)malloc(n, sizeof(int*));
+	A = (int**)calloc(n, sizeof(int*));
 
 	for (i = 0; i < n; i++) {
-		A[i] = (int*)malloc(m, sizeof(int);
+		A[i] = (int*)calloc(m, sizeof(int));
 		for (j = 0; j < m; j++) {
 			scanf("%d", &A[i][j]);
 		}
 	}
-	scnf("%d %d", &m, &k);
 
-	B = (int**)malloc(n, sizeof(int*));
+	B = (int**)calloc(m, sizeof(int*));
 
 	for (i = 0; i < m; i++) {
-		B[i] = (int*)malloc(k, sizeof(int));
+		B[i] = (int*)calloc(k, sizeof(int));
 		for (j = 0; j < k; j++) {
-			scanf("%d", *B[i][j]);
+			scanf("%d", &B[i][j]);
 		}
 	}
 
 	for (i = 0; i < n; i++) {
-		for (j = 0; j < m; j++) {
+		for (j = 0; j < k; j++) {
 			Cij = 0;
 			for (p = 0; p < m; p++) {
 				Cij += A[i][p] * B[p][j];
-				printf("%d", Cij);
 			}
+			printf("%d ", Cij);
 		}
+		printf("\n");
 	}
 	for (i = 0; i < n; i++) {
 		free(A[i]);
-	} free(A);
+	}
+	free(A);
+
 	for (i = 0; i < m; i++) {
 		free(B[i]);
-	} free(B);
+	}
+	free(B);
+
 	return 0;
 }
